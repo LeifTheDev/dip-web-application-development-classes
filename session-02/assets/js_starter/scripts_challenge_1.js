6946a2577155133a3889ff55141912832e78967a
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-07-28 13:31:57 +0800

    feat: Week 2 activity 2

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -1,8 +1,10 @@
 // Challenge 1 Starter
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5];
+let numbersCopy = [...numbers];
 
 // Use the pop() method to remove the last element from the array
+<<<<<<< HEAD
 let lastElement = numbers.pop();
 
 // Use the unshift() method to add the last element to the beginning of the array
@@ -14,3 +16,17 @@ let newArray = numbers.slice(3);
 // Set the value of the output field inner html to the new array
 let outField = document.getElementById("Output field");
 outputField.innerHTML = newArray;
+=======
+let deletedNumber = numbersCopy.pop();
+
+// Use the unshift() method to add the last element to the beginning of the array
+numbersCopy.unshift(deletedNumber);
+
+// Use the slice() method to exclude the first 3 elements and return a new array
+let outputNumbers = numbersCopy.slice(3);
+
+// Set the value of the output field inner html to the new array
+let outputField = document.getElementById("OutputField");
+
+outputField.innerHTML = outputNumbers;
+>>>>>>> bea7e64 (feat: Week 2 activity 2)
