71d49e10cc425c84fe042f2c943a2ae6e66124c1
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-07-28 11:46:19 +0800

    feat: Resolve merge

--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -1,5 +1,15 @@
-// Challenge 5 Solution
-// Initialise the array
+// // Challenge 5 Solution
+// // Initialise the array
+// let numbers = [1, 2, 3, 4, 5];
+
+// // Define the function doubleNumbers
+
+// // Call the function with the numbers array
+// let result =  doubleNumbers(numbers);
+
+// // Console log the new array
+// console.log(result);
+
 let numbers = [1, 2, 3, 4, 5];
 
 // Define the function doubleNumbers
