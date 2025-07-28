9a817e03729bad3832abff667125917f2076833d
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-07-28 11:46:19 +0800

    feat: Resolve merge

--- session-01/assets/js_starter/scripts_challenge_2.js
+++ session-01/assets/js_starter/scripts_challenge_2.js
@@ -4,9 +4,11 @@ let numbers = [1, 2, 3, 4, 5];
 
 // Define the arrow function doubleNumbers
 
-
+doubleNumbers = (numbers) => {
+    return numbers.map((number) => number * 2);
+}
 // Call the function with the numbers array
 
-
+result = doubleNumbers(numbers);
 // Console log the new array
 console.log(result);
