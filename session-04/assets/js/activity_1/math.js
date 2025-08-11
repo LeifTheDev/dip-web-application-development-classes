77c7024ee008f0ef0e94f365dc3dd8927a90b326
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-08-11 12:52:46 +0800

    feat: Session 4 activity 1

--- session-04/assets/js/activity_1/math.js
+++ session-04/assets/js/activity_1/math.js
@@ -6,10 +6,6 @@ export const subtract = (a, b) => a - b;
 
 export const divide = (a, b) => a / b;
 
-<<<<<<< HEAD
 export default function multiply(a, b) {
   return a * b;
 }
-=======
-export default (a, b) => a * b;
->>>>>>> 251ea2d (activty 1 update)
