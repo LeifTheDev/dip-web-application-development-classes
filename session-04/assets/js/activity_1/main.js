9d8403c4a2b51374040f25c57c20962ec39de1c2
LeifTheDev <161671624+LeifTheDev@users.noreply.github.com>
2025-08-11 12:52:46 +0800

    feat: Session 4 activity 1

--- session-04/assets/js/activity_1/main.js
+++ session-04/assets/js/activity_1/main.js
@@ -1,4 +1,27 @@
-import { add, subtract } from "./math.js";
+import multiply, { add, subtract, divide as myDivide } from "./math.js";
 
-console.log(add(3, 2)); // 5
-console.log(subtract(5, 2)); // 3
+let outputSection = document.getElementById("results");
+
+// Class not required in this case, should just be a function really. (Class used for syntax practice)
+class DisplayTemplate {
+  parse = function (value) {
+    let parser = new DOMParser();
+    return parser.parseFromString(`<p>${value} - Template Used<p>`, "text/html")
+      .body.firstChild;
+    // let element = document.createElement("");
+    // element.innerHTML = `<p>${this.value}<p>`;
+    // return element;
+  };
+}
+
+let templateGenerator = new DisplayTemplate();
+
+function renderResult(value) {
+  let node = templateGenerator.parse(value);
+  outputSection.appendChild(node);
+}
+
+renderResult(`Multiply: ${multiply(3, 4)}`);
+renderResult(`Add: ${add(3, 4)}`);
+renderResult(`Subtract: ${subtract(4, 1)}`);
+renderResult(`Divide: ${myDivide(20, 4)}`);
