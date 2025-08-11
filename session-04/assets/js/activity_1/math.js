26e4602c63baee4c55ded97e9aa07af967580bc5
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-08-11 12:56:14 +0800

    activty 1 update

--- session-04/assets/js/activity_1/math.js
+++ session-04/assets/js/activity_1/math.js
@@ -1,10 +1,15 @@
 export const add = function (a, b) {
   return a + b;
 };
+
 export const subtract = (a, b) => a - b;
 
 export const divide = (a, b) => a / b;
 
+<<<<<<< HEAD
 export default function multiply(a, b) {
   return a * b;
 }
+=======
+export default (a, b) => a * b;
+>>>>>>> 251ea2d (activty 1 update)
