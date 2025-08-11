7b7228403e3b4f8dccbaaad2487e0b0957016370
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-08-11 12:02:43 +0800

    feat: load and remove storage

--- session-03/assets/js_starter/scripts_challenge_4.js
+++ session-03/assets/js_starter/scripts_challenge_4.js
@@ -9,18 +9,10 @@ let prefix = "autosave_";
  * @param  {Event} event The event object
  */
 function inputHandler(event) {
-<<<<<<< HEAD
-  localStorage.setItem(prefix + event.target.id, event.target.value);
-  // switch (event.target.id) {
-  //   case "name": {
-  //   }
-  // }
-=======
   // save the event target
   let field = event.target;
 
   localStorage.setItem(prefix + field.id, field.value);
->>>>>>> f8dffb3 (feat: input handler)
 }
 
 /**
@@ -38,6 +30,7 @@ function clearStorage() {
  * Load saved data from localStorage
  */
 function loadSaved() {
+  //
   let fields = form.elements;
 
   for (let field of fields) {
@@ -49,12 +42,13 @@ function loadSaved() {
 }
 
 // Load saved data from localStorage
-<<<<<<< HEAD
+function loadSaved() {
+    if (!saved) {continue };
+    field.value = saved;
+}
+
 loadSaved();
 
-=======
-function loadSaved() {}
->>>>>>> f8dffb3 (feat: input handler)
 // Listen for DOM events
 form.addEventListener("input", inputHandler);
 form.addEventListener("submit", clearStorage);
